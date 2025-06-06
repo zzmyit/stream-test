d542a739fca5fbc363b49ad729e68c218bd7edf5
zzmyit <71055651+zzmyit@users.noreply.github.com>
2025-06-06 23:16:08 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "stra"         # 用户名
+UUID = "277cc408-95e1-40f2-9caf-caa739702269"                     # UUID，留空则自动生成
+PORT = 8001                   # Vmess端口，留空或0则自动生成
+DOMAIN = "stra.air.now.cc"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiOGY2ZmZiODM1MzVmYmQ2Yjg5M2Y5ODY3NjJmZjQwMjQiLCJ0IjoiNzZkMzAwMTctZjg5YS00ZjQ4LThmZTEtMjkzNTQxMWUzMTcyIiwicyI6IlptWTFNREpoTnpjdE9UUTFOUzAwWkRCakxXSTFabVl0Wm1KaVlqSXhOVE15Tm1RdyJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
